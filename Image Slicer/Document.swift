1fb85713853525ed3d1001bed0a2f7465e38a9cd
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-02-13 15:53:51 -0500

    Document: Un/pickled document

--- Image Slicer/Document.swift
+++ Image Slicer/Document.swift
@@ -9,6 +9,11 @@
 import Cocoa
 
 class Document: NSDocument {
+    var job = Job(image: nil) {
+        didSet {
+            updateViewControllerJob()
+        }
+    }
 
     override init() {
         super.init()
@@ -25,25 +30,55 @@ class Document: NSDocument {
     }
 
     override func makeWindowControllers() {
-        // Returns the Storyboard that contains your Document window.
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
         let windowController = storyboard.instantiateControllerWithIdentifier("Document Window Controller") as! NSWindowController
         self.addWindowController(windowController)
+
+        updateViewControllerJob()
+    }
+
+    func updateViewControllerJob() {
+        if let
+            windowController = self.windowControllers.first,
+            vc = windowController.contentViewController as? ViewController {
+                vc.job = self.job
+        }
     }
 
     override func dataOfType(typeName: String) throws -> NSData {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let dict = job.asDictionary
+        let data = NSKeyedArchiver.archivedDataWithRootObject(dict)
+        return data
     }
 
     override func readFromData(data: NSData, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        guard let something = NSKeyedUnarchiver.unarchiveObjectWithData(data) else {
+            throw NSError(domain: "blargh", code: 1, userInfo:  [
+                NSLocalizedDescriptionKey: "Failed to unarchive data",
+                ])
+        }
+
+        guard let dict = something as? [String: AnyObject] else {
+            throw NSError(domain: "blargh", code: 1, userInfo:  [
+                NSLocalizedDescriptionKey: "Unarchived thing wasn't a dictionary",
+                NSLocalizedFailureReasonErrorKey: "Was: \(something)",
+                ])
+        }
+
+        guard let job = Job(dictionary: dict) else {
+            throw NSError(domain: "blargh", code: 1, userInfo:  [
+                NSLocalizedDescriptionKey: "Job unpickling failed",
+                ])
+        }
+
+        self.job = job
     }
 
+
+//    override func restoreDocumentWindowWithIdentifier(identifier: String, state: NSCoder, completionHandler: (NSWindow?, NSError?) -> Void) {
+//        NSLog("DEBUG: state restoration disabled")
+//        completionHandler(nil, nil)
+//    }
 
 }
 
