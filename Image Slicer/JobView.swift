c489604b95ee13bc4e1868d7841a5d16cd06c97e
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-02-14 11:54:58 -0500

    JobView: Add editing mode

--- Image Slicer/JobView.swift
+++ Image Slicer/JobView.swift
@@ -1,7 +1,32 @@
 import Cocoa
 
+enum EditingMode {
+    case NotEditing
+    case AddingCut(Orientation)
+    case AddingMark
+    case DeletingCut
+    case DeletingMark
+}
+
 class JobView: NSImageView {
     var job = Job(image: nil, cuts: [], selections: [])
+
+    var editingMode = EditingMode.NotEditing {
+        didSet {
+            guard self.editable else {
+                editingMode = .NotEditing
+                return
+            }
+
+            if case .NotEditing = editingMode {
+                didFinishEditing(self)
+            }
+        }
+    }
+
+    var didFinishEditing: (JobView) -> Void = { _ in
+        return
+    }
 
     override var image: NSImage? {
         didSet {
@@ -26,6 +51,7 @@ class JobView: NSImageView {
         super.init(coder: coder)
     }
 }
+
 
 
 // MARK: - Drawing
@@ -64,5 +90,68 @@ extension JobView {
             let text = selection.name
             text.drawAtPoint(selection.around, withAttributes: attributes)
         }
+    }
+}
+
+
+
+// MARK: - Editing
+extension JobView {
+    override func mouseDown(theEvent: NSEvent) {
+        let windowPoint = theEvent.locationInWindow
+        let point = convertPoint(windowPoint, fromView: nil)
+
+        if let mode = performEdit(point) {
+            editingMode = mode
+            needsDisplay = true
+        }
+    }
+
+
+    /// - returns: next mode to change to, if any
+    func performEdit(point: CGPoint) -> EditingMode? {
+        switch editingMode {
+        case .NotEditing:
+            return nil
+
+        case let .AddingCut(orientation):
+            job.add(Cut(at: point, oriented: orientation))
+            return .NotEditing
+
+        case .AddingMark:
+            let name = "Subimage \(job.selections.count + 1)"
+            job.selections.append(ExportSelection(around: point, name: name))
+            return .NotEditing
+
+        case .DeletingCut:
+            if let hitPoint = nearest(point, amongst: job.cuts.map { $0.at }),
+                index = job.cuts.indexOf({ $0.at == hitPoint }) {
+                    job.cuts.removeAtIndex(index)
+                    return .NotEditing
+            }
+
+        case .DeletingMark:
+            if let hitPoint = nearest(point, amongst: job.selections.map { $0.around }),
+                index = job.selections.indexOf({ $0.around == hitPoint }) {
+                    job.selections.removeAtIndex(index)
+                    return .NotEditing
+            }
+        }
+
+        fatalError("somehow made it past an exhaustive switch-case with editingMode: \(editingMode)")
+    }
+
+
+    /// - returns: optional since `points` might be empty
+    func nearest(target: CGPoint, amongst points: [CGPoint]) -> CGPoint? {
+        let pointsAndDistances = points.map { point -> (CGPoint, CGFloat) in
+            let dx = point.x - target.x
+            let dy = point.y - target.y
+            return (point, dx*dx + dy*dy)
+        }
+        let pointAndMinDistance = pointsAndDistances.minElement { (left, right) -> Bool in
+            return left.1 < right.1
+        }
+        return pointAndMinDistance?.0
     }
 }
