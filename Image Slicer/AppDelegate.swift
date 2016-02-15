7909fc383646e152bdf0c96872ae2735f275888b
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-02-14 23:58:37 -0500

    Handle dropped-on images

--- Image Slicer/AppDelegate.swift
+++ Image Slicer/AppDelegate.swift
@@ -7,19 +7,58 @@
 //
 
 import Cocoa
-import ImageSlicing
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    func application(sender: NSApplication, openFiles filenames: [String]) {
+        NSLog("asked to open files: \(filenames.joinWithSeparator("\n- "))")
+        for file in filenames {
+            guard openFile(file) else {
+                NSApp.replyToOpenOrPrint(.Failure)
+                return
+            }
+        }
+        NSApp.replyToOpenOrPrint(.Success)
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    func openFile(file: String) -> Bool {
+        let controller = NSDocumentController.sharedDocumentController()
+
+        let URL = NSURL(fileURLWithPath: file)
+        if let image = NSImage(contentsOfURL: URL) {
+            do {
+                let someDocument = try controller.makeUntitledDocumentOfType(Document.nativeType)
+                guard let myDocument = someDocument as? Document else {
+                    NSLog("\(someDocument) has wrong class: opened \(file)")
+                    return false
+                }
+
+                controller.addDocument(someDocument)
+
+                myDocument.job = Job(image: image)
+                if let imageName = URL.URLByDeletingPathExtension?.lastPathComponent {
+                    myDocument.setDisplayName(imageName)
+                }
+                myDocument.makeWindowControllers()
+                myDocument.showWindows()
+                return true
+            } catch {
+                NSLog("failed creating untitled document: \(error)")
+                return false
+            }
+        }
+
+        let type = try! controller.typeForContentsOfURL(URL)
+        NSLog("\(file): has type \(type)")
+        controller.openDocumentWithContentsOfURL(URL, display: true) {
+        (document, ok, error) -> Void in
+            let result: AnyObject? = ok ? document : error
+            NSLog("opening \(file): \(ok) \(result)")
+
+            if let error = error {
+                NSApp.presentError(error)
+            }
+        }
+        return true
     }
-
-
 }
-
