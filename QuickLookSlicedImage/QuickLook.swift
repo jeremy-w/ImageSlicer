03a81bae825902d63a0a626ffcd7293c935124f6
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-03-05 19:13:18 -0500

    Plough through naive thumbnail implementation
    We stand up our JobView and draw it. I haven't tested this yet, so not
    sure it works, but it certainly compiles, for what that's worth.

--- QuickLookSlicedImage/QuickLook.swift
+++ QuickLookSlicedImage/QuickLook.swift
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import Foundation
+import Cocoa
 import QuickLook
 
 class Thumbnail: NSObject {
@@ -23,5 +23,22 @@ class Thumbnail: NSObject {
 
     @objc
     func makeThumbnail(atMost size: CGSize, with request: QLThumbnailRequestRef, options: NSDictionary) {
+        guard let view = JobView(job: document.job) else {
+            NSLog("\(self): failed to create job view for job: \(document.job) - document: \(document)")
+            return
+        }
+
+        let rect = view.bounds
+        let forDrawingBitmap = true
+        let properties = NSDictionary()
+        guard let unmanagedContext = QLThumbnailRequestCreateContext(request, rect.size, forDrawingBitmap, properties)  else {
+            NSLog("\(self): failed to create drawing context with size \(rect.size) - document: \(document)")
+            return
+        }
+
+        let rawContext = unmanagedContext.takeRetainedValue()
+        let context = NSGraphicsContext(CGContext: rawContext, flipped: true)
+        view.displayRectIgnoringOpacity(rect, inContext: context)
+        QLThumbnailRequestFlushContext(request, rawContext)
     }
 }
